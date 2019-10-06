f683f5fc3904493d6d4bd795164c18bc33ff50af
David Guan <davidguandev@gmail.com>
2019-10-07 08:41:23 +1100

    rm old TODO

--- voice-notes.js
+++ voice-notes.js
@@ -28,9 +28,6 @@ async function createVoiceNote(title, content, userId) {
     .promise();
 
   const taskId = result.SynthesisTask.TaskId;
-  // TODO: periodically call `getSpeechSynthesisTask`
-  // to check whether the task is really completed successfully,
-  // and return the result only after that.
   notes.push({ name: title, key: `${taskId}.mp3`, userId: userId });
   console.log("Successfully sent a Polly task for ", title, "at:", new Date());
 
