b7e38ff29626a8f1709e496359bc982997d064fb
David Guan <davidguandev@gmail.com>
2019-10-08 21:45:07 +1100

    add loaders for js and css

--- webpack.config.js
+++ webpack.config.js
@@ -1,15 +1,46 @@
 const { CleanWebpackPlugin } = require("clean-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 const HtmlWebpackPlugin = require("html-webpack-plugin")
 const path = require("path");
 
 module.exports = {
   entry: "./client/main.js",
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: [
+          {
+            loader: MiniCssExtractPlugin.loader,
+          },
+          "css-loader"
+        ]
+      },
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: ["@babel/preset-env"]
+          }
+        }
+      }
+    ]
+  },
   output: {
     filename: "[name].[contenthash].js",
     path: path.resolve(__dirname, "dist")
   },
   plugins: [
     new CleanWebpackPlugin(),
+    new MiniCssExtractPlugin({
+      // Options similar to the same options in webpackOptions.output
+      // all options are optional
+      filename: "[name].[hash].css",
+      chunkFilename: "[id].css",
+      ignoreOrder: false // Enable to remove warnings about conflicting order
+    }),
     new HtmlWebpackPlugin({
       template: "client/index.template.html",
       filename: "../index.html"
